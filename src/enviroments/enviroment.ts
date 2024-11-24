5e17c5ad814a3031a69cd2f9d6a0eb67abfd7871
Hirushan99 <palihewadana11716@usci.ruh.ac.lk>
2024-11-24 09:54:19 +0530

    feat:setting firebase environment

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDbokrhjAeWEaeXfGTPc6SazyCuxqEjuvY",
+    authDomain: "wingrow-sl.firebaseapp.com",
+    projectId: "wingrow-sl",
+    storageBucket: "wingrow-sl.firebasestorage.app",
+    messagingSenderId: "739794403629",
+    appId: "1:739794403629:web:6b4b60b381e54d50c213a4",
+    measurementId: "G-ZNZ6RXZ6JN"
+  }
+};
