7ba9e99389636795a5644c93c3b6820792f2c52a
Hirushan99 <palihewadana11716@usci.ruh.ac.lk>
2024-11-25 20:15:31 +0530

    feat:create main category component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainCategoryComponent } from '../pages/main-category/main-category.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MainCategoryComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MainCategoryComponent } from '../pages/main-category/main-category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'main-category',
+    component: MainCategoryComponent
+  }
+];
